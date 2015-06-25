52d82eccef4d0d28016a5b39a93d47e30c46f293
Joel Ng <blood_elves88@yahoo.com.sg>
2015-06-25 12:44:04 +0800

    Fixed converted time display. Fixed obtaining of current time offset.

--- app/src/main/java/com/example/android/timeconverter/MainActivity.java
+++ app/src/main/java/com/example/android/timeconverter/MainActivity.java
@@ -46,31 +46,11 @@ public class MainActivity extends ActionBarActivity {
 
     public void convertTime(View view) {
 
-        TimeZone targetTimeZone;
         long timeZoneId = getTimeZoneId();
         Calendar date = getDateAndTime();
 
-        TimeZone currentTimeZone = TimeZone.getDefault();
-        int currentTimeOffset = currentTimeZone.getOffset(GregorianCalendar.AD, date.get(Calendar.YEAR),
-                date.get(Calendar.MONTH),
-                date.get(Calendar.DAY_OF_MONTH),
-                date.get(Calendar.DAY_OF_WEEK),
-                date.get(Calendar.MILLISECOND));
-
-        if (timeZoneId == 0) {
-            targetTimeZone = TimeZone.getTimeZone("GMT-8:00");
-        } else if (timeZoneId == 1) {
-            targetTimeZone = TimeZone.getTimeZone("GMT-7:00");
-        } else {
-            targetTimeZone = TimeZone.getTimeZone("GMT+8:00");
-        }
-
-        int targetTimeOffset = targetTimeZone.getOffset(GregorianCalendar.AD,
-                date.get(Calendar.YEAR),
-                date.get(Calendar.MONTH),
-                date.get(Calendar.DAY_OF_MONTH),
-                date.get(Calendar.DAY_OF_WEEK),
-                date.get(Calendar.MILLISECOND));
+        int currentTimeOffset = getCurrentTimeOffset();
+        int targetTimeOffset = getTargetTimeOffset(timeZoneId, date);
 
         int adjustment = currentTimeOffset - targetTimeOffset;
         date.add(Calendar.MILLISECOND, +adjustment);
@@ -80,18 +60,50 @@ public class MainActivity extends ActionBarActivity {
         setDateText(date);
     }
 
-    private void setTimeText(Calendar date) {
-        TextView convertedTimeText = (TextView)findViewById(R.id.converted_time);
-        if (date.get(Calendar.MINUTE) == 0) {
-            convertedTimeText.setText("" + date.get(Calendar.HOUR) + ":" + date.get(Calendar.MINUTE) + "0");
-        } else if (date.get(Calendar.MINUTE) < 10) {
-            convertedTimeText.setText("" + date.get(Calendar.HOUR) + ":0" + date.get(Calendar.MINUTE));
+    private int getTargetTimeOffset(long timeZoneId, Calendar date) {
+        TimeZone targetTimeZone;
+        if (timeZoneId == 0) {
+            targetTimeZone = TimeZone.getTimeZone("GMT-8:00");
+        } else if (timeZoneId == 1) {
+            targetTimeZone = TimeZone.getTimeZone("GMT-7:00");
         } else {
-            convertedTimeText.setText("" + date.get(Calendar.HOUR) + ":" + date.get(Calendar.MINUTE));
+            targetTimeZone = TimeZone.getTimeZone("GMT+8:00");
         }
 
-        if (date.get(Calendar.HOUR) == 0) {
-            convertedTimeText.setText("12:" + date.get(Calendar.MINUTE) + "0");
+        return targetTimeZone.getOffset(GregorianCalendar.AD,
+                date.get(Calendar.YEAR),
+                date.get(Calendar.MONTH),
+                date.get(Calendar.DAY_OF_MONTH),
+                date.get(Calendar.DAY_OF_WEEK),
+                date.get(Calendar.MILLISECOND));
+    }
+
+    private int getCurrentTimeOffset() {
+        Calendar today = Calendar.getInstance();
+        TimeZone currentTimeZone = TimeZone.getDefault();
+        return currentTimeZone.getOffset(GregorianCalendar.AD, today.get(Calendar.YEAR),
+                today.get(Calendar.MONTH),
+                today.get(Calendar.DAY_OF_MONTH),
+                today.get(Calendar.DAY_OF_WEEK),
+                today.get(Calendar.MILLISECOND));
+    }
+
+    private void setTimeText(Calendar date) {
+        TextView convertedTimeText = (TextView)findViewById(R.id.converted_time);
+
+        int hour = date.get(Calendar.HOUR);
+        int minute = date.get(Calendar.MINUTE);
+
+        if (hour == 0) {
+            hour = 12;
+        }
+
+        if (minute == 0) {
+            convertedTimeText.setText(hour + ":" + minute + "0");
+        } else if (minute < 10) {
+            convertedTimeText.setText(hour + ":0" + minute);
+        } else {
+            convertedTimeText.setText(hour + ":" + minute);
         }
 
         if (date.get(Calendar.AM_PM) == Calendar.AM) {
